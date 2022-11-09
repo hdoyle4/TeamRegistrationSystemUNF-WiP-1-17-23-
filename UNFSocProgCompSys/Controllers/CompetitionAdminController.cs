6117606333c4034a74f8c27e89756b93054482cd
Indiana Ludwig <83787968+IndianaLudwig@users.noreply.github.com>
2022-11-09 13:25:34 -0500

    Fixing the location of views and controller that I recently created and adjusting existing competition views to reflect the wireframes.

---
+++ UNFSocProgCompSys/Controllers/CompetitionAdminController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UNFSocProgCompSys.Data;
+using UNFSocProgCompSys.Models;
+
+namespace UNFSocProgCompSys.Controllers
+{
+    public class CompetitionAdminController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CompetitionAdminController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Page to view competition list and manage competitions (Admin)
+        public IActionResult CompetitionManagement()
+        {
+            IEnumerable<Competition> CompetitionList = _db.Competitions;
+            return View(CompetitionList);
+        }
+        //Page to create a new competition (Admin)
+        public IActionResult CompetitionCreate()
+        {
+            return View();
+        }
+        //Page to edit an existing competition (Admin)
+        public IActionResult CompetitionEdit(Guid? CompetitionID)
+        {
+            if (CompetitionID == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var CurrentCompetition = _db.Competitions.Find(CompetitionID);
+
+                if (CurrentCompetition == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(CurrentCompetition);
+                }
+            }
+        } //End of CompetitionEdit
+    }     //End of class CompetitionAdminController
+}         //End of namespace UNFSocProgCompSys.Controllers
