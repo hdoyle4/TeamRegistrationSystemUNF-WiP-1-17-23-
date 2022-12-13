6af922cf731189489e8ffd5421909ae3ab9ad870
zjf1018 <jingfengzheng@icloud.com>
2022-12-12 23:13:12 -0500

    D3 update

---
+++ UNFSocProgCompSys/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UNFSocProgCompSys.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult UserList()
+        {
+            var users = userManager.Users;
+            return View(users);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("UserList");
+            }
+            else
+            {
+                var result = await userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("UserList");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("UserList");
+            }
+        }
+    }
+}
