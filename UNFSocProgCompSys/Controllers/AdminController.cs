349a4e1565fe888676ff5b7553ddb112c7e0eb3f
HENRIMUKAJ <95234885+HENRIMUKAJ@users.noreply.github.com>
2022-12-13 00:30:04 -0500

    added admin services

--- UNFSocProgCompSys/Controllers/AdminController.cs
+++ UNFSocProgCompSys/Controllers/AdminController.cs
@@ -1,54 +1,75 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using UNFSocProgCompSys.Models;
+using UNFSocProgCompSys.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UNFSocProgCompSys.Controllers
 {
     public class AdminController : Controller
     {
-        private readonly UserManager<IdentityUser> userManager;
-
-        public AdminController(UserManager<IdentityUser> userManager)
+        private readonly IAdminServices _AdminService;
+        public AdminController(IAdminServices adminServices)
         {
-            this.userManager = userManager;
+            _AdminService = adminServices;
         }
 
         [HttpGet]
-        public IActionResult UserList()
+        public async Task<IActionResult> UserList1()
         {
-            var users = userManager.Users;
-            return View(users);
+            var users = await _AdminService.GetUsers();
+         
+            var model = new ProfileView()
+            {
+                UserProfile = users
+            };
+            return View(model);
         }
+
+        //[HttpGet]
+        //public IActionResult UserList()
+        //{
+        //    var users = userManager.Users;
+        //    return View(users);
+        //}
+
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
+        //[HttpPost]
         public async Task<IActionResult> DeleteUser(string id)
         {
-            var user = await userManager.FindByIdAsync(id);
-
-            if (user == null)
-            {
-                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
-                return View("UserList");
-            }
-            else
-            {
-                var result = await userManager.DeleteAsync(user);
-
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("UserList");
-                }
-
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }
-
-                return View("UserList");
-            }
+            var success = await _AdminService.DeleteUserById(id);
+            return View(UserList1);
         }
+
+        //[HttpPost]
+        //public async Task<IActionResult> DeleteUser(string id)
+        //{
+        //    var user = await userManager.FindByIdAsync(id);
+
+        //    if (user == null)
+        //    {
+        //        ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+        //        return View("UserList");
+        //    }
+        //    else
+        //    {
+        //        var result = await userManager.DeleteAsync(user);
+
+        //        if (result.Succeeded)
+        //        {
+        //            return RedirectToAction("UserList");
+        //        }
+
+        //        foreach (var error in result.Errors)
+        //        {
+        //            ModelState.AddModelError("", error.Description);
+        //        }
+
+        //        return View("UserList");
+        //    }
+        //}
     }
 }
---
+++ UNFSocProgCompSys/Services/AdminServices.cs
@@ -0,0 +1,31 @@
+﻿using UNFSocProgCompSys.Data;
+using UNFSocProgCompSys.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace UNFSocProgCompSys.Services
+{
+    public class AdminServices : IAdminServices
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User[]> GetUsers()
+        {
+            return await _context.Users.Where(x => x.UserName != null).ToArrayAsync();
+        }
+
+        public async Task<bool> DeleteUserById(string id)
+        {
+           var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+           _context.Users.Remove(user);
+           _context.SaveChangesAsync();
+            return true;
+        }
+
+        
+    }
+}
